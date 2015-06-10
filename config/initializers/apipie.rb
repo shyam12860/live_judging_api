ddde04269f56e9dae02e05a496a363f3deb671d2
Stephen Dolan <stephen@neadwerx.com>
2015-06-10 01:53:40 -0400

    Add Disqus to API

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,6 +2,7 @@ Apipie.configure do |config|
   config.app_name                = "LiveJudging"
   config.api_base_url            = ""
   config.doc_base_url            = "/apipie"
+  config.disqus_shortname        = "live-judge-api"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
 end
