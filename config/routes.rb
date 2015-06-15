55a50a0d757dfcde8c43e8f95c6d29845ff0b07b
Stephen Dolan <stephen@neadwerx.com>
2015-06-15 19:21:15 -0400

    CORS OPTION preflight fix attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   constraints subdomain: 'api' do
+    # Need to match all routes that are using the options
+    match ":asterisk", via: [ :options ]
+
     apipie
     root to: 'apipie/apipies#index'
 
     get 'login', to: 'sessions#create'
     get 'logout', to: 'sessions#destroy'
 
-    resources :users, only: [:index, :create, :show, :update]
+    resources :users,  only: [:index, :create, :show, :update]
     resources :events, only: [:index, :create, :show, :update]
-    resources :roles, only: [:index, :show]
+    resources :roles,  only: [:index, :show]
   end
 end
