51c304e3c7c9f174d99615cf7095d1b68e55a4a6
Stephen Dolan <stephen@neadwerx.com>
2015-06-20 18:22:50 -0400

    Update event serializer to return an array of judge ids

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,6 +2,7 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :name, :location, :start_time, :end_time
 
   has_many :organizers, embed: :ids
+  has_many :judges, embed: :ids
 
   def start_time
     object.start_time.strftime( "%F %r" )
