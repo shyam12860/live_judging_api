dd10be31f333d9a1ce0bb8aff02595c65d360c5d
Stephen Dolan <stephen@neadwerx.com>
2015-06-24 16:24:52 -0400

    Another attempted fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,11 @@ class ApplicationController < ActionController::API
   include Pundit
 
   before_action :authenticate
-  after_filter :set_cors_headers
   after_action :verify_authorized, except: :index
+
+  before_filter :cors_preflight_check
+  before_filter :allow_cross_domain_access
+  after_filter :set_cors_access_control
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
@@ -55,10 +58,27 @@ class ApplicationController < ActionController::API
       StringIO.new( Base64.decode64( string ) )
     end
 
-    def set_cors_headers
+    def cors_preflight_check
+      if request.method == :options
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
+        headers['Access-Control-Max-Age'] = '1728000'
+        render :text => '', :content_type => 'text/plain'
+      end
+    end
+
+    def set_cors_access_control
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
       headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
       headers['Access-Control-Max-Age'] = "1728000"
+    end
+
+    def allow_cross_domain_access
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
+      headers['Access-Control-Allow-Headers'] = 'GET, POST, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Max-Age'] = '1728000'
     end
 end
