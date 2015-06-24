02aabcc9a1e031920827eed9b35aff844c3a4b4f
Stephen Dolan <stephen@neadwerx.com>
2015-06-24 15:46:16 -0400

    Revert

--- config/application.rb
+++ config/application.rb
@@ -10,21 +10,21 @@ module LiveJudging
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
 
-    #config.middleware.insert_before ActionDispatch::Static, "Rack::Cors", :logger => (-> { Rails.logger } ) do
-    #  allow do
-    #    origins '*'
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
 
-    #    resource '/cors',
-    #      :headers => :any,
-    #      :methods => [:post],
-    #      :credentials => true,
-    #      :max_age => 0
+        resource '/cors',
+          :headers => :any,
+          :methods => [:post],
+          :credentials => true,
+          :max_age => 0
 
-    #    resource '*',
-    #      :headers => :any,
-    #      :methods => [:get, :post, :delete, :put, :options, :head],
-    #      :max_age => 0
-    #  end
-    #end
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :options, :head],
+          :max_age => 0
+      end
+    end
   end
 end
