63f27d7f47cb15cf03f2bae762138fca39940b17
Stephen Dolan <dolan.stephen1@gmail.com>
2015-07-18 11:47:47 -0400

    Fix include statement typo

--- app/policies/criterion_policy.rb
+++ app/policies/criterion_policy.rb
@@ -1,6 +1,6 @@
 class CriterionPolicy < ApplicationPolicy
   def index?
-    user.present? && ( record.first.rubric.event.organizers.include?( user ) || record.first.rubric.event.judges.include( user ) )
+    user.present? && ( record.first.rubric.event.organizers.include?( user ) || record.first.rubric.event.judges.include?( user ) )
   end
 
   def show?
