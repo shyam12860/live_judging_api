bf32f12107b9581fe039126424616961cc16a877
Stephen Dolan <dolan.stephen1@gmail.com>
2015-07-21 14:27:48 -0400

    Change name of application

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'live_judging'
+set :application, 'live_judging_api'
 set :repo_url, 'git@github.com:stephendolan/live_judging_api.git'
-set :deploy_to, '/home/deploy/live_judging'
+set :deploy_to, '/home/deploy/live_judging_api'
 
 set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
